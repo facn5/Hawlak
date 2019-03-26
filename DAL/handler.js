cd8bd6daf44a6d1a8bf5ac246df11740c1b7bc71
faris <farisabutaya@gmail.com>
2019-03-26 15:36:14 +0200

    added css, added endpoints to server.js,added json file, fixed small bug Relates #4

--- DAL/handler.js
+++ DAL/handler.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
+// const rest=require('/../DB/restList.js')
 const handler = (request, response) => {
   const endpoint = request.url;
   if (endpoint === '/') {
@@ -10,12 +10,26 @@ const handler = (request, response) => {
         response.writeHead(500);
         response.end("500 error");
       } else {
-        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
         response.end(file);
       };
     })
+  } else if (endpoint === '/style.css') {
+    fs.readFile(__dirname + "/../UI/style.css", function(error, file) {
+      if (error) {
+        response.writeHead(500)
+        response.end("500 csafoasfg")
+      } else {
+        response.writeHead(200, {
+          "Content-Type": "text/css"
+        });response.end(file);
+      }
+    })
   }
 }
+
 
 
 
