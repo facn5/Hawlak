af1890fde2f3833fda9f5b649ef484d02274ac09
majdya <majd.ya.1995@gmail.com>
2019-03-28 11:35:59 +0200

    t3

--- DAL/server.js
+++ DAL/server.js
@@ -1,10 +1,15 @@
 const http = require('http');
 // const host = process.env.HOST || 'localhost';
-var port = process.env.PORT || 4000;
+// var port = process.env.PORT || 4000;
 const router = require('./router.js');
 
 const server = http.createServer(router);
 
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 server.listen(port);
 
 console.log('server running on port 4000');
